b2fca0fb51efab7cc0a2b74f0bc11a4f6295a723
Tayyab Xtreme <tayyabxtrem@gmail.com>
2024-02-12 08:53:29 +0500

    add mouse listener class

--- src/main/java/Xtrem/Window.java
+++ src/main/java/Xtrem/Window.java
@@ -4,6 +4,7 @@ import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
@@ -29,6 +30,16 @@ public class Window {
         System.out.println("Hello LWJGL"+ Version.getVersion()+"!");
         init();
         loop();
+
+        // Free the memory
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        // Terminate GLFW and the free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+
+
 
     }
     public void init(){
