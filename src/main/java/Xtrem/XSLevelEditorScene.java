cd0117118b1bdf4ce4fe2f52899f3e363a584def
Tayyab Xtreme <tayyabxtrem@gmail.com>
2024-02-13 06:14:02 +0500

    texture loading feature

--- src/main/java/Xtrem/XSLevelEditorScene.java
+++ src/main/java/Xtrem/XSLevelEditorScene.java
@@ -5,6 +5,7 @@ import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL20;
 import renderer.Shader;
+import renderer.Texture;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -17,11 +18,11 @@ public class XSLevelEditorScene extends XSScene {
     private int vertexID, fragmentID, shaderProgram;
 
     private float[] vertexArray = {
-            // position               // color
-             100.5f,-100.5f,0.0f,       1.0f, 0.0f, 0.0f, 1.0f, // Bottom right 0
-            -100.5f, 100.5f ,0.0f,       0.0f, 1.0f, 0.0f, 1.0f, // Top left     1
-             100.5f, 100.5f ,0.0f,       1.0f, 0.0f, 1.0f, 1.0f, // Top right    2
-            -100.5f,-100.5f ,0.0f,       1.0f, 1.0f, 0.0f, 1.0f, // Bottom left  3
+            // position               // color                  // UV Coordinates
+            100f,   0f, 0.0f,       1.0f, 0.0f, 0.0f, 1.0f,     1, 1, // Bottom right 0
+            0f, 100f, 0.0f,       0.0f, 1.0f, 0.0f, 1.0f,     0, 0, // Top left     1
+            100f, 100f, 0.0f ,      1.0f, 0.0f, 1.0f, 1.0f,     1, 0, // Top right    2
+            0f,   0f, 0.0f,       1.0f, 1.0f, 0.0f, 1.0f,     0, 1  // Bottom left  3
     };
 
     // IMPORTANT: Must be in counter-clockwise order
@@ -36,6 +37,8 @@ public class XSLevelEditorScene extends XSScene {
 
     private int vaoID, vboID, eboID;
     private Shader defaultShader;
+    private Texture testTextture;
+
 
 
     public XSLevelEditorScene() {
@@ -45,11 +48,11 @@ public class XSLevelEditorScene extends XSScene {
     }
     public void init(){
 
-        this.camera=new XSCamera(new Vector2f());
+        this.camera = new XSCamera(new Vector2f(-200, -300));
 
        defaultShader = new Shader("assets/Shaders/default.glsl");
        defaultShader.compile();
-
+        this.testTextture=new Texture("assets/images/testImage.jpg");
 
 
 
@@ -79,20 +82,29 @@ public class XSLevelEditorScene extends XSScene {
         // Add the vertex attribute pointers
         int positionsSize = 3;
         int colorSize = 4;
-        int floatSizeBytes = 4;
-        int vertexSizeBytes = (positionsSize + colorSize) * floatSizeBytes;
+        int uvSize = 2;
+        int vertexSizeBytes = (positionsSize + colorSize + uvSize) * Float.BYTES;
         glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeBytes, 0);
         glEnableVertexAttribArray(0);
 
-        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * floatSizeBytes);
+        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * Float.BYTES);
         glEnableVertexAttribArray(1);
+
+        glVertexAttribPointer(2, uvSize, GL_FLOAT, false, vertexSizeBytes, (positionsSize + colorSize) * Float.BYTES);
+        glEnableVertexAttribArray(2);
     }
 
     @Override
     public void update(float dt) {
-        camera.position.x-=dt * 50.0f;
-        camera.position.y-=dt * 50.0f;
+     //   camera.position.x-=dt * 50.0f;
+     //   camera.position.y-=dt * 50.0f;
         defaultShader.use();
+
+        defaultShader.uploadTexture("TEX_SAMPLER",0);
+        glActiveTexture(GL_TEXTURE);
+        testTextture.bind();
+
+
         defaultShader.uploadMat4f("uProjection",camera.getProjectionMatrix());
         defaultShader.uploadMat4f("uView",camera.getViewMatrix());
         defaultShader.uploadFloat("uTime", Time.getTime());
